134d4711127559216cc7517e7d1c2a11f182e82a
petr-step <1stepanekpetr@gmail.com>
2021-03-16 13:46:05 +0100

    classes created

---
+++ DataLoaderNoSQL/src/DataLoaderNoSQL/DataMapper.java
@@ -0,0 +1,14 @@
+package DataLoaderNoSQL;
+
+import java.util.ArrayList;
+
+public class DataMapper {
+	
+	public static ArrayList<Zamestnanec> getZamestnanci() {
+		//TODO
+	}
+	
+	public static ArrayList<VykazPrace> getVykazyPrace() {
+		//TODO
+	}
+}
