f061fd15da6447840589a2bfc2b2a734b02d494a
petr-step <1stepanekpetr@gmail.com>
2021-03-16 13:41:52 +0100

    package created

--- DataLoaderNoSQL/src/DataLoaderNoSQL/DataLoader.java
+++ DataLoaderNoSQL/src/DataLoaderNoSQL/DataLoader.java
@@ -1,3 +1,4 @@
+package DataLoaderNoSQL;
 
 public class DataLoader {
 
---
+++ DataLoaderNoSQL/src/DataLoaderNoSQL/MongoWriter.java
@@ -0,0 +1,12 @@
+package DataLoaderNoSQL;
+
+public class MongoWriter {
+	
+	public static void connectToDb() {
+		//TODO
+	}
+	
+	public static void writeZamestnanec() {
+		
+	}
+}
